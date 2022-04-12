d4bc6a0d86fb878ecf5d70b795f6ed28dfa2fb64
Subhan Khan <subhank1997@gmail.com>
2022-04-12 12:41:51 -0400

    commit tree c409b901cdb7db5844ae2330c94e4df1618f4788

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b){
+   return a+b;
+}
+function subtract(b,a){
+   return b-a;
+}
+function multiply(a,b){
+   return a*b;
+}
+function divide(a,b){
+   return a/b;
+}
+function increment(a){
+   return (a += 1);
+}
+function decrement(a){
+   return (a -= 1);
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+} 
+function preserveDecimal(n) {
+   return parseFloat (n);
+}
+
